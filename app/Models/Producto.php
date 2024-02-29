7e377b44c89b68c22074bbc441a0e80c9b5bbdbc
Adria Lasala <[adria.lasala2907@gmail.com]>
2024-02-29 17:13:16 +0100

    Prueba de push

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,18 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         "nombre",
         "descripcion",
         "precio",
         "idCategoria"
     ];
+
     public function pedidos()
     {
-        return $this->belongsToMany(Pedido::class,'pedido_productos');
+        return $this->belongsToMany(Pedido::class, 'pedido_productos');
     }
+
     public function categorias()
     {
         return $this->belongsTo(Categoria::class);
     }
-    //prueba
+}
