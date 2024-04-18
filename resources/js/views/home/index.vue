1ffabf783451d234ba34e1788ffe97cd959a6f9d
Adria Lasala <[adria.lasala2907@gmail.com]>
2024-04-18 19:07:22 +0200

    Formulario ed regsitro en home funcional, seccion 1 feta, seccion 2 estructurada

--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -15,20 +15,62 @@
                         <img src="/images/prueba_clavo.svg" class="clavo_img" alt="cartel de se busca">
                         <h3>¿Aún no has desembarcado?</h3>
                         <h4>Regístrate gratis</h4>
-                        <form>
-                            <div class="form-group">
-                                <label for="nombre">Nombre:</label>
-                                <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                        <form @submit.prevent="submitRegister">
+                            <div class="">
+                                <!-- Email -->
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">{{ $t('name') }}</label>
+                                    <input v-model="registerForm.name" id="name" type="text" class="form-control" autofocus>
+                                    <!-- Validation Errors -->
+                                    <div class="text-danger mt-1">
+                                        <div v-for="message in validationErrors?.name">
+                                            {{ message }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">{{ $t('email') }}</label>
+                                    <input v-model="registerForm.email" id="email" type="email" class="form-control" autocomplete="username">
+                                    <!-- Validation Errors -->
+                                    <div class="text-danger mt-1">
+                                        <div v-for="message in validationErrors?.email">
+                                            {{ message }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Password -->
+                                <div class="mb-4">
+                                    <label for="password" class="form-label">
+                                        {{ $t('password') }}
+                                    </label>
+                                    <input v-model="registerForm.password" id="password" type="password" class="form-control" autocomplete="current-password">
+                                    <!-- Validation Errors -->
+                                    <div class="text-danger-600 mt-1">
+                                        <div v-for="message in validationErrors?.password">
+                                            {{ message }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <label for="password_confirmation" class="form-label">
+                                        {{ $t('confirm_password') }}
+                                    </label>
+                                    <input v-model="registerForm.password_confirmation" id="password_confirmation" type="password" class="form-control" autocomplete="current-password">
+                                    <!-- Validation Errors -->
+                                    <div class="text-danger-600 mt-1">
+                                        <div v-for="message in validationErrors?.password_confirmation">
+                                            {{ message }}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Buttons -->
+                                <div class="flex items-center justify-end mt-4">
+                                    <button class="btn btn-primary" :class="{ 'opacity-25': processing }" :disabled="processing">
+                                        {{ $t('register') }}
+                                    </button>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="email">Email:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Contraseña:</label>
-                                <input type="password" class="form-control" id="password" placeholder="Cotnraseña">
-                            </div>
-                            <button type="submit" class="btn btn-primary mt-3">Enviar</button>
                         </form>
                     </div>
                 </div>
@@ -40,6 +82,26 @@
         <div class="mt-5">
             <img src="/images/separador.svg" alt="Separador" >
         </div>
+        <div class="card">
+            <Carousel>
+                <template>
+                    <div class="border-1 surface-border border-round m-2  p-3">
+                        <div class="mb-3">
+                            <div class="relative mx-auto">
+                            </div>
+                        </div>
+                        <div class="mb-3 font-medium">ASDASDASd</div>
+                        <div class="flex justify-content-between align-items-center">
+                            <div class="mt-0 font-semibold text-xl">12€</div>
+                            <span>
+                            <Button icon="pi pi-heart" severity="secondary" outlined />
+                            <Button icon="pi pi-shopping-cart" class="ml-2"/>
+                        </span>
+                        </div>
+                    </div>
+                </template>
+            </Carousel>
+        </div>
     </section>
 
 </template>
@@ -47,6 +109,9 @@
 <script setup>
 import axios from 'axios';
 import {ref, onMounted} from 'vue'
+import useAuth from '@/composables/auth'
+
+const { registerForm, validationErrors, processing, submitRegister } = useAuth();
 
 const posts = ref();
 
@@ -55,6 +120,8 @@ onMounted(() => {
         posts.value = data;
     })
 })
+
+//Carrousel script
 </script>
 
 <style scoped>
@@ -72,6 +139,7 @@ onMounted(() => {
     padding: 20px;
     max-width: 500px; /* Ancho máximo del contenedor */
     margin: 0 auto; /* Centrado horizontal */
+    transform: rotate(5deg);
 }
 .clavo_img{
     width:32px;
